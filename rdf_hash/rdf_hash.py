49a6f79534c44b2f0eeae9db882d15d193d632c4
Stefano Cossu <scossu@artic.edu>
2018-05-09 23:14:07 -0500

    Use a class to store state of the current subject.

--- rdf_hash/rdf_hash.py
+++ rdf_hash/rdf_hash.py
@@ -2,187 +2,195 @@ from hashlib import sha256
 
 from rdflib import URIRef, Literal, BNode
 
-__doc__ = """
-Generate a canonical serialization and cryptographic hash of a RDF graph.
 
-From the original paper by Edzard Höfig and Ina Schieferdecker, whose
-initials are used to name this class:
-
-http://ceur-ws.org/Vol-1259/method2014_submission_1.pdf
-
-**Note:** This implementation represents RDF identifiers in their full N3
-syntax: fully qualified IRIs are represented with angle brackets, literals
-with double quotes, and so on, e.g.::
-
-    <http://ex.org/a> # An IRI
-    "Ahoy"^^<http://www.w3.org/2001/XMLSchema#string> # A string literal
-    3 # An xsd:int literal
-
-**Note:** there has been further development of this logic since the original
-publication. Most notably, prof. Miguel Ceriani discovered two edge cases that
-invalidate the logic. This implementation attempts to resolve the first one but
-the second one will require further research. See
-https://groups.google.com/d/msg/fedora-tech/8pemDHNvbvc/KLp5633jBgAJ
-"""
-
-S_START = b'{'
-"""Start of subject block."""
-
-S_END = b'}'
-"""End of subject block."""
-
-P_START = b'('
-"""Start of predicate block."""
-
-P_END = b')'
-"""End of predicate block."""
-
-O_START = b'['
-"""Start of object block."""
-
-O_END = b']'
-"""End of object block."""
-
-BNODE = b'*'
-"""Blank node symbol."""
-
-SELF_REF_BNODE = b'!'
-"""
-Self-referential blank node.
-
-This is an addition to the original logic to address an edge case brought up by
-prof. Miguel Ceriani with self-referential blank nodes.
-
-    Graph 1::
-
-       _:a ex:p _:c .
-       _:b ex:p _:c .
-       _:c ex:p _:c .
-
-    Graph 2::
-
-       _:a ex:p _:b .
-       _:b ex:p _:a .
-       _:c ex:p _:c .
-
-    The two RDF graphs represented are different (and not just for the
-    used labels) but the algorithm generates in both cases the following
-    string (spaces added for readability)::
-
-       { * [ - ( * [ - ] ) ] } { * [ - ( * [ - ] ) ] } { * [ - ] }
-
-"""
-
-
-def get_hash(gr, hash_fn=sha256):
+class EhisCanon:
     """
-    Calculate the hash of an RDF graph.
+    Generate a canonical serialization and cryptographic hash of a RDF graph.
 
-    This is an optional function. A custom hash function can either be passed
-    to this function, or the ``serialize`` function can be used to serialize
-    the graph into canonical form without hashing it.
+    From the original paper by Edzard Höfig and Ina Schieferdecker, whose
+    initials are used to name this class:
 
-    :param rdflib.Graph gr: Input graph.
-    :param function hash_fn: Hash function. Defaults to ``hashlib.sha256``.
+    http://ceur-ws.org/Vol-1259/method2014_submission_1.pdf
 
-    :rtype: bytes
-    :return: Graph digest.
+    **Note:** This implementation represents RDF identifiers in their full N3
+    syntax: fully qualified IRIs are represented with angle brackets, literals
+    with double quotes, and so on, e.g.::
+
+        <http://ex.org/a> # An IRI
+        "Ahoy"^^<http://www.w3.org/2001/XMLSchema#string> # A string literal
+        3 # An xsd:int literal
+
+    **Note:** there has been further development of this logic since the original
+    publication. Most notably, prof. Miguel Ceriani discovered two edge cases that
+    invalidate the logic. This implementation attempts to resolve the first one but
+    the second one will require further research. See
+    https://groups.google.com/d/msg/fedora-tech/8pemDHNvbvc/KLp5633jBgAJ
     """
-    return hash_fn(serialize(gr)).digest()
 
+    S_START = b'{'
+    """Start of subject block."""
 
-def serialize(gr):
+    S_END = b'}'
+    """End of subject block."""
+
+    P_START = b'('
+    """Start of predicate block."""
+
+    P_END = b')'
+    """End of predicate block."""
+
+    O_START = b'['
+    """Start of object block."""
+
+    O_END = b']'
+    """End of object block."""
+
+    BNODE = b'*'
+    """Blank node symbol."""
+
+    ORIG_S = b'!'
     """
-    Generate a canonical serialization of an RDF graph.
+    Original subject node.
 
-    :param rdflib.Graph gr: Input graph.
+    This is an addition to the original logic to address an edge case brought up by
+    prof. Miguel Ceriani with self-referential blank nodes.
 
-    :rtype: bytearray
-    :return: Serialized graph.
+        Graph 1::
+
+           _:a ex:p _:c .
+           _:b ex:p _:c .
+           _:c ex:p _:c .
+
+        Graph 2::
+
+           _:a ex:p _:b .
+           _:b ex:p _:a .
+           _:c ex:p _:c .
+
+        The two RDF graphs represented are different (and not just for the
+        used labels) but the algorithm generates in both cases the following
+        string (spaces added for readability)::
+
+           { * [ - ( * [ - ] ) ] } { * [ - ( * [ - ] ) ] } { * [ - ] }
+
     """
-    subj_strings = []
-    res = bytearray()
-    for subject in set(gr.subjects()):
-        visited_nodes = set()
-        subj_strings.append(
-                _encode_subject(subject, visited_nodes, gr))
 
-    subj_strings.sort()
-    for subj_str in subj_strings:
-        res += S_START
-        res += subj_str
-        res += S_END
-
-    return res
+    _orig_subj = None
 
 
-def _encode_subject(subject, visited_nodes, gr):
-    """
-    Encode a subject node.
+    def get_hash(self, gr, hash_fn=sha256):
+        """
+        Calculate the hash of an RDF graph.
 
-    :param subject: Subject node to encode
-    :type subject: rdflib.URIRef or rdflib.BNode
-    :param list visited_nodes: registry of visited blank nodes.
-    :param rdflib.Graph gr: Input graph.
+        This is an optional function. A custom hash function can either be passed
+        to this function, or the ``serialize`` function can be used to serialize
+        the graph into canonical form without hashing it.
 
-    :rtype: bytes
-    :return: Encoded bytestring.
-    """
-    if isinstance(subject, BNode):
-        if subject in visited_nodes:
-            return b''
+        :param rdflib.Graph gr: Input graph.
+        :param function hash_fn: Hash function. Defaults to ``hashlib.sha256``.
+
+        :rtype: bytes
+        :return: Graph digest.
+        """
+        return hash_fn(self.serialize(gr)).digest()
+
+
+    def serialize(self, gr):
+        """
+        Generate a canonical serialization of an RDF graph.
+
+        :param rdflib.Graph gr: Input graph.
+
+        :rtype: bytearray
+        :return: Serialized graph.
+        """
+        subj_strings = []
+        res = bytearray()
+        self._orig_subj = None
+
+        for subject in set(gr.subjects()):
+            visited_nodes = set()
+            self._orig_subj = subject
+            #import pdb; pdb.set_trace()
+            subj_strings.append(
+                    self._encode_subject(subject, visited_nodes, gr))
+
+        subj_strings.sort()
+        for subj_str in subj_strings:
+            res += self.S_START
+            res += subj_str
+            res += self.S_END
+
+        return res
+
+
+    def _encode_subject(self, subject, visited_nodes, gr):
+        """
+        Encode a subject node.
+
+        :param subject: Subject node to encode
+        :type subject: rdflib.URIRef or rdflib.BNode
+        :param list visited_nodes: registry of visited blank nodes.
+        :param rdflib.Graph gr: Input graph.
+
+        :rtype: bytes
+        :return: Encoded bytestring.
+        """
+        if isinstance(subject, BNode):
+            if subject in visited_nodes:
+                return self.ORIG_S if subject == self._orig_subj else b''
+            else:
+                visited_nodes.add(subject)
+                res = bytearray(self.BNODE)
         else:
-            visited_nodes.add(subject)
-            res = bytearray(BNODE)
-    else:
-        res = bytearray(subject.n3().encode('utf-8'))
-    res += _encode_props(subject, visited_nodes, gr)
+            res = bytearray(subject.n3().encode('utf-8'))
+        res += self._encode_props(subject, visited_nodes, gr)
 
-    return res
+        return res
 
 
-def _encode_props(subject, visited_nodes, gr):
-    """
-    encode properties (predicates + objects) for a subject.
+    def _encode_props(self, subject, visited_nodes, gr):
+        """
+        encode properties (predicates + objects) for a subject.
 
-    :param subject: Subject node with properties to encode
-    :type subject: rdflib.URIRef or rdflib.BNode
-    :param list visited_nodes: registry of visited blank nodes.
-    :param rdflib.Graph gr: Input graph.
+        :param subject: Subject node with properties to encode
+        :type subject: rdflib.URIRef or rdflib.BNode
+        :param list visited_nodes: registry of visited blank nodes.
+        :param rdflib.Graph gr: Input graph.
 
-    :rtype: bytes
-    :return: Encoded bytestring.
-    """
-    res = bytearray()
-    preds = sorted(set(gr.predicates(subject)))
-    for pred in preds:
-        res += P_START
-        res += pred.n3().encode('utf-8')
-        objs = sorted([
-                _encode_object(obj, visited_nodes, gr)
-                for obj in set(gr.objects(subject, pred))])
-        for obj in objs:
-            res += O_START
-            res += obj
-            res += O_END
-        res += P_END
-    return res
+        :rtype: bytes
+        :return: Encoded bytearray.
+        """
+        res = bytearray()
+        preds = sorted(set(gr.predicates(subject)))
+        for pred in preds:
+            res += self.P_START
+            res += pred.n3().encode('utf-8')
+            objs = sorted([
+                    self._encode_object(obj, visited_nodes, gr)
+                    for obj in set(gr.objects(subject, pred))])
+            for obj in objs:
+                res += self.O_START
+                res += obj
+                res += self.O_END
+            res += self.P_END
+        return res
 
 
-def _encode_object(obj, visited_nodes, gr):
-    """
-    Encode an object node.
+    def _encode_object(self, obj, visited_nodes, gr):
+        """
+        Encode an object node.
 
-    :param rdflib.term.Identifier obj: Object node.
-    :param list visited_nodes: registry of visited blank nodes.
-    :param rdflib.Graph gr: Input graph.
+        :param rdflib.term.Identifier obj: Object node.
+        :param list visited_nodes: registry of visited blank nodes.
+        :param rdflib.Graph gr: Input graph.
 
-    :rtype: bytes
-    :return: Encoded bytestring.
-    """
-    if isinstance(obj, BNode):
-        return _encode_subject(obj, visited_nodes, gr)
-    else:
-        # This includes both Literal and URIRef cases.
-        return obj.n3().encode('utf-8')
+        :rtype: bytes
+        :return: Encoded bytestring.
+        """
+        if isinstance(obj, BNode):
+            return self._encode_subject(obj, visited_nodes, gr)
+        else:
+            # This includes both Literal and URIRef cases.
+            return obj.n3().encode('utf-8')
 
